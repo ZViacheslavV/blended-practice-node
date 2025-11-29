c7229c8514d93ee8020ba001b9601b9986540416
ZViacheslavV <vvvv.zkv@gmail.com>
2025-11-29 19:56:07 +0200

    add all functions

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,7 +1,10 @@
+import createHttpError from 'http-errors';
 import {
   createProduct,
+  deleteProduct,
   getProductById,
   getProducts,
+  updateProduct,
 } from '../services/products.js';
 
 export const getAllProducts = async (req, res) => {
@@ -16,8 +19,9 @@ export const getAllProducts = async (req, res) => {
 
 export const getProductByIdController = async (req, res) => {
   const { productId } = req.params;
-
   const product = await getProductById(productId);
+
+  if (!product) throw createHttpError(404, 'Product not found');
 
   res.json({
     status: 200,
@@ -32,6 +36,32 @@ export const createProductController = async (req, res) => {
   res.json({
     status: 201,
     message: 'Successfully created product!',
+    data: product,
+  });
+};
+
+export const updateProductController = async (req, res) => {
+  const { productId } = req.params;
+  const product = await updateProduct(productId, req.body);
+
+  if (!product) throw createHttpError(404, 'Product not found');
+
+  res.json({
+    status: 200,
+    message: 'Successfully updated product!',
+    data: product,
+  });
+};
+
+export const deleteProductController = async (req, res) => {
+  const { productId } = req.params;
+  const product = await deleteProduct(productId);
+
+  if (!product) throw createHttpError(404, 'Product not found');
+
+  res.json({
+    status: 200,
+    message: 'Successfully deleted product!',
     data: product,
   });
 };
--- src/services/products.js
+++ src/services/products.js
@@ -6,3 +6,9 @@ export const getProductById = (productId) =>
   ProductModel.findOne({ _id: productId });
 
 export const createProduct = (payload) => ProductModel.create(payload);
+
+export const updateProduct = (productId, payload) =>
+  ProductModel.findOneAndUpdate({ _id: productId }, payload, { new: true });
+
+export const deleteProduct = (productId) =>
+  ProductModel.findOneAndDelete({ _id: productId });
