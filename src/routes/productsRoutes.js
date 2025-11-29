961b6bdefd6fc1031a3eb44ad8a2d97a3606e43a
ZViacheslavV <vvvv.zkv@gmail.com>
2025-11-29 19:58:37 +0200

    fix

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import {
   createProductController,
+  deleteProductController,
   getAllProducts,
   getProductByIdController,
+  updateProductController,
 } from '../controllers/productsController.js';
 
 const productRouter = Router();
@@ -10,5 +12,7 @@ const productRouter = Router();
 productRouter.get('/products', getAllProducts);
 productRouter.get('/products/:productId', getProductByIdController);
 productRouter.post('/products', createProductController);
+productRouter.patch('/products/:productId', updateProductController);
+productRouter.delete('/products/:productId', deleteProductController);
 
 export default productRouter;
