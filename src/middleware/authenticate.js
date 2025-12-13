551870264b267a67b662698990e4a54c187ccd3f
ZVyachelsavV <vvvv.zkv@gmail.com>
2025-12-13 21:55:12 +0200

    add authenticate middleware

---
+++ src/middleware/authenticate.js
@@ -0,0 +1,25 @@
+import createHttpError from 'http-errors';
+import { Session } from '../models/session.js';
+import { User } from '../models/user.js';
+
+export const authenticate = async (req, res, next) => {
+  const accessToken = req.cookies;
+
+  if (!accessToken) throw createHttpError(401, 'Missing access token');
+
+  const session = await Session.findOne({ accessToken });
+
+  if (!session) throw createHttpError(401, 'Session not found');
+
+  const isAccessTokenExpired = session.accessTokenValidUntil < new Date();
+  if (isAccessTokenExpired) throw createHttpError(401, 'Access token expired');
+
+  const user = User.findById(session.userId);
+
+  if (!user)
+    throw createHttpError(401, 'No session associated with this token found');
+
+  req.user = user;
+
+  next();
+};
